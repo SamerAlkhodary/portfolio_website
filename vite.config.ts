85271b56d62609067f7ac610a119bb0ba8b14b95
Samer Alkhodary <samkhud@hotmail.com>
2024-07-25 16:03:06 +0200

    hack: for 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path';
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
     base: '/portfolio_website/',
     plugins: [react(), viteTsconfigPaths()],
     server: {    
@@ -12,4 +12,12 @@ export default defineConfig({
         // this sets a default port to 3000  
         port: 3000, 
     },
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            404: resolve(__dirname, "public/404.html"),
+          },
+        },
+    }
 })
